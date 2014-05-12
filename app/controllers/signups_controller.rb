2f15e6cc4524283c661606b247d2af69fde15f37
github_name <github_email>
2014-05-12 13:05:04 -0400

    form done

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,15 @@
 class SignupsController < ApplicationController
+  def new
+    @signup = Signup.new
+  end
+  def create
+    @user = Signup.new(signup_params)
+    # @user.save ?  :
+  end
+
+  private
+
+  def signup_params
+    params.require(:signup).permit(:first_name, :last_name, :email, :company, :phone_number)
+  end
 end
